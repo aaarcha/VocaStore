cfb975585a5d778732c1f6bac2f684ea0a3e162e
A <abandoallyza05@gmail.com>
2026-05-14 02:25:56 +0800

    ui

--- backend/app.py
+++ backend/app.py
@@ -8,7 +8,7 @@ import os
 # -------------------------
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 PROJECT_ROOT = os.path.dirname(BASE_DIR)   # 👈 IMPORTANT FIX
-FRONTEND_DIR = os.path.join(os.path.dirname(__file__), "C:\\Users\\Admin\\Downloads\\VocaStore\\frontend")
+FRONTEND_DIR = os.path.join(os.path.dirname(__file__), "../frontend")
 
 app = Flask(__name__, static_folder=FRONTEND_DIR, static_url_path="")
 CORS(app)
