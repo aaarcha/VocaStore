24ac1bc86facb3be54c46e5223b1d6368872de4d
A <abandoallyza05@gmail.com>
2026-05-14 00:31:21 +0800

    railway deployment

--- backend/db.py
+++ backend/db.py
@@ -1,12 +1,12 @@
 import os
 import psycopg2
 
+
 def get_connection():
     return psycopg2.connect(
-        host=os.getenv("postgres.railway.internal"),
-        database=os.getenv("railway"),
-        user=os.getenv("postgres"),
-        password=os.getenv("ykuiSgkzVVPlSddFKWgBBLMhimNtnwYQ"),
-        port=os.getenv("5432", 5432)
+        host=os.getenv("PGHOST"),
+        database=os.getenv("PGDATABASE"),
+        user=os.getenv("PGUSER"),
+        password=os.getenv("PGPASSWORD"),
+        port=os.getenv("PGPORT")
     )
-    
