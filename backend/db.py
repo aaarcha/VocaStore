dc4a560b3a9d91698ef9cbbead3d2124dbab1783
A <abandoallyza05@gmail.com>
2026-05-13 18:42:43 +0800

    for railway

--- backend/db.py
+++ backend/db.py
@@ -1,10 +1,11 @@
+import os
 import psycopg2
 
 def get_connection():
     return psycopg2.connect(
-        host="localhost",
-        port=5432,
-        database="vocastore_db",
-        user="postgres",
-        password="postgres"
+        host=os.getenv("postgres.railway.internal"),
+        database=os.getenv("railway"),
+        user=os.getenv("postgres"),
+        password=os.getenv("ykuiSgkzVVPlSddFKWgBBLMhimNtnwYQ"),
+        port=os.getenv("5432", 5432)
     )
