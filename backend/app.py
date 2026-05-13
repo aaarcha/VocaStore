52cdd5a84e08c146f41c50b74a675819f6e75a1f
A <abandoallyza05@gmail.com>
2026-05-14 00:27:04 +0800

    railway deployment

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,10 @@
-from flask import Flask
+from flask import Flask, jsonify
+from flask_cors import CORS
+from db import get_connection
 
 app = Flask(__name__)
+CORS(app)
+
 
 @app.route("/")
 def home():
@@ -9,7 +13,30 @@ def home():
 
 @app.route("/products")
 def products():
-    return {
+
+    conn = get_connection()
+    cur = conn.cursor()
+
+    cur.execute("""
+        SELECT id, name, price, stock
+        FROM products
+        ORDER BY id
+    """)
+
+    rows = cur.fetchall()
+
+    cur.close()
+    conn.close()
+
+    return jsonify({
         "success": True,
-        "data": []
-    }
+        "data": [
+            {
+                "id": r[0],
+                "name": r[1],
+                "price": float(r[2]),
+                "stock": r[3]
+            }
+            for r in rows
+        ]
+    })
