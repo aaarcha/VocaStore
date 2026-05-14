67c2bad8e05786ad0416acb5f8a7e77ba9cf2187
A <abandoallyza05@gmail.com>
2026-05-14 15:40:49 +0800

    pgvector ai

--- backend/services/text_normalizer.py
+++ backend/services/text_normalizer.py
@@ -1,14 +1,33 @@
 NUMBER_MAP = {
     "isa": "1",
+    "isang": "1",
+
     "dalawa": "2",
+    "dalawang": "2",
+
     "tatlo": "3",
+    "tatlong": "3",
+
     "apat": "4",
+    "apat na": "4",
+
     "lima": "5",
+    "limang": "5",
+
     "anim": "6",
+    "anim na": "6",
+
     "pito": "7",
+    "pitong": "7",
+
     "walo": "8",
+    "walong": "8",
+
     "siyam": "9",
-    "sampu": "10"
+    "siyam na": "9",
+
+    "sampu": "10",
+    "sampung": "10"
 }
 
 def normalize_numbers(text):
--- backend/services/vector_store.py
+++ backend/services/vector_store.py
@@ -1,42 +1,5 @@
-from backend.services.embedding_service import generate_embedding
-
 def save_command_embedding(db, text):
-
-    embedding = generate_embedding(text)
-
-    conn = db()
-    cur = conn.cursor()
-
-    cur.execute("""
-        INSERT INTO command_embeddings
-        (command_text, embedding)
-        VALUES (%s, %s)
-    """, (text, embedding))
-
-    conn.commit()
-
-    cur.close()
-    conn.close()
-
+    return
 
 def search_similar_command(db, text):
-
-    embedding = generate_embedding(text)
-
-    conn = db()
-    cur = conn.cursor()
-
-    cur.execute("""
-        SELECT command_text,
-               embedding <=> %s::vector AS distance
-        FROM command_embeddings
-        ORDER BY distance
-        LIMIT 1
-    """, (embedding,))
-
-    result = cur.fetchone()
-
-    cur.close()
-    conn.close()
-
-    return result
+    return None
